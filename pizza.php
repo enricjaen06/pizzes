401696f4f7de9fe25bd50517fb7cbd20ae3efd8d
enricjaen06 <448417.clot@fje.edu>
2026-02-16 14:53:06 +0000

    Actualització de pizza.php i pizza.html per deploy automàtic

--- pizza.php
+++ pizza.php
@@ -1,18 +1,17 @@
 <html>
  <head>
  <meta content="text/html; charset=UTF-8" http-equiv="content-type">
- <title>FES LA TEVA PIZZA2</title>
+ <title>FES LA TEVA PIZZA</title>
  </head>
  <body>
  <?php
  $preu = 8;
  $IVA = 21;
-
  if (empty($_GET['ingr'])) {
-     $pvp = ((100 + $IVA) / 100) * $preu;
+     $pvp = ((100 + $IVA)/100)*$preu;
      echo "Has demanat només la pizza bàsica. El preu és $pvp € (IVA inclòs)<br>";
  } else {
-     $pvp = ((100 + $IVA) / 100) * ($preu + count($_GET['ingr']));
+     $pvp = ((100 + $IVA)/100)*($preu+count($_GET['ingr']));
      echo "Has demanat la pizza bàsica amb:<br>";
      foreach ($_GET['ingr'] as $ingredient) {
          if ($ingredient) {
@@ -21,10 +20,8 @@
      }
      echo "El preu és $pvp € (IVA inclòs)<br>";
  }
-
  echo "<br>";
  echo "Autor: enricjaen06<br>";
  ?>
  </body>
 </html>
-
